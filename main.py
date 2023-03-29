a4b3dd252f8517fba8a5555e7c416ae1016727b7
AlexisDupont2 <816522@mystma.org>
2023-03-29 15:01:13 -0500

    5

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+#WRITE YOUR CODE IN THIS FILE // numL >= 1:
 def hasL(w):
-    for i in range(0,len(w),1):
+    numL = 0
+    for i in range(0,len(w)):
+        w[i] == "l"
+        numL = numL + 1
         if w[i] == "l":
             return True
-        else:
-            return False
-hasL("dog")
-hasL("alabama")
+    else:
+        return False
+print(hasL("dog"))
+print(hasL("alabama"))
+
 
